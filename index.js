1cb5341dcdb2e29332acfc4c11c47948199e730d
Nina Hahne <ninamariehahne@gmail.com>
2020-04-25 16:45:42 +0200

    tidying

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // const cowsay = require("cowsay");
-const cors = require("cors");
+// const cors = require("cors");
 const path = require("path");
 
 // Create the server
@@ -10,27 +10,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use(express.json());
-
-// Serve our api route /cow that returns a custom talking text cow
-// app.get("/api/cow/:say", cors(), async (req, res, next) => {
-//   try {
-//     const text = req.params.say;
-//     const moo = cowsay.say({ text });
-//     res.json({ moo });
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-// Serve our base route that returns a Hellow World cow
-// app.get("/api/cow/", cors(), async (req, res, next) => {
-//   try {
-//     const moo = cowsay.say({ text: "Hello World!" });
-//     res.json({ moo });
-//   } catch (err) {
-//     next(err);
-//   }
-// });
 
 app.get("/open", (req, res) => {
   console.log("*************** /open ***********");
