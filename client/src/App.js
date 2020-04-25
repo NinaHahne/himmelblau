6b2fdd7e5edcb15e625640d0433d7b45aaf2ce01
Nina Hahne <ninamariehahne@gmail.com>
2020-04-25 15:58:35 +0200

    Deploy takeoff

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+// import axios from "./axios";
+import logo from "./himmelblau-logo.png";
+import "./App.css";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    // if you don't use arrow functions to pass functions to children, you need to bind this:
+    // this.setBio = this.setBio.bind(this);
+  }
+  componentDidMount() {
+    this.getOpeningHours();
+  }
+  getOpeningHours() {
+    console.log('I will get the current opening hours now :)');
+    // axios.get("/open").then(({ data }) => {
+    //   // console.log('data from get /open: ', data);
+    //   this.setState(data);
+    //   // console.log("state of app: ", this.state);
+    // });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
+}
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, "client/build")));
 
+app.use(express.json());
+
 // Serve our api route /cow that returns a custom talking text cow
 // app.get("/api/cow/:say", cors(), async (req, res, next) => {
 //   try {
@@ -30,14 +32,37 @@ app.use(express.static(path.join(__dirname, "client/build")));
 //   }
 // });
 
+app.get("/open", (req, res) => {
+  console.log("*************** /open ***********");
+  getOpeningHours()
+    .then(rows => {
+      // check if rows[0].length != 0;
+      res.json({
+        success: true,
+        userId: rows[0].id,
+        first: rows[0].first,
+        last: rows[0].last,
+        imageUrl: rows[0].img_url || "/images/duck-308733.png",
+        bio: rows[0].bio || ""
+      });
+    })
+    .catch(err => {
+      console.log("err in GET /open: ", err);
+      res.json({
+        success: false
+      });
+    });
+});
+
 // Anything that doesn't match the above, send back the index.html file
 app.get("*", (req, res) => {
   // res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  res.send('Blumenladen himmelblau');
+  // res.send("Blumenladen himmelblau");
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 // Choose the port and start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Mixing it up on port ${PORT}`);
 });
