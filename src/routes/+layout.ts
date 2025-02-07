7ea5db9f3d5c9a4c34cdb652d897fbd1619f9668
NinaHahne <ninamariehahne@gmail.com>
2025-02-07 18:03:53 +0100

    WIP: language support

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,9 @@
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = ({ params }) => {
+  const supportedLanguages = ['de', 'en'];
+  const lang = supportedLanguages.includes(params.lang) ? params.lang : 'de'; // Default to 'de' if unsupported
+  return { lang };
+};
+
 export const prerender = true;
