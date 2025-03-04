f0eb0b41a19f512be1ead12e11f5a3bc26d1c8f2
NinaHahne <ninamariehahne@gmail.com>
2025-03-04 22:28:37 +0100

    WIP: animated svg logo

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+  import AnimatedLogo from "$lib/components/AnimatedLogo.svelte";
+</script>
+
+<AnimatedLogo />
+
 <h1>Coming Soon: Impressum</h1>
 <p>This page is under construction.</p>
