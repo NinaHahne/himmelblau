99df5f367f14604f38750c207c4b0ea25ae732cb
NinaHahne <ninamariehahne@gmail.com>
2025-03-05 20:35:46 +0100

    animated 'himmelblau' handwriting

--- src/routes/himmelblau/+page.svelte
+++ src/routes/himmelblau/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+  import AnimatedLogo from "$lib/components/AnimatedLogo.svelte";
+</script>
+
+<AnimatedLogo />
+
+<br />
 <h1>Coming Soon: Himmelblau</h1>
 <p>This page is under construction.</p>
--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -1,8 +1,2 @@
-<script lang="ts">
-  import AnimatedLogo from "$lib/components/AnimatedLogo.svelte";
-</script>
-
-<AnimatedLogo />
-
 <h1>Coming Soon: Impressum</h1>
 <p>This page is under construction.</p>
