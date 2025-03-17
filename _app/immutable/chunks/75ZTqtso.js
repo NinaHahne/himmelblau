151eb34035369b38026d6f7bce345c5d7f139e4b
NinaHahne <ninamariehahne@gmail.com>
2025-03-17 17:50:01 +0100

    Updates

--- _app/immutable/chunks/75ZTqtso.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1590xug)==null?void 0:s.base)??"/himmelblau";var a;const t=((a=globalThis.__sveltekit_1590xug)==null?void 0:a.assets)??e;export{t as a,e as b};
--- _app/immutable/entry/start.BNc16CSF.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/Bo_mqxvT.js";export{t as start};
