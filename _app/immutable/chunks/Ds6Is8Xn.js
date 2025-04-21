b625be51db9b117e01d51d7cad919697ccf52638
NinaHahne <ninamariehahne@gmail.com>
2025-04-21 21:17:02 +0200

    Updates

--- _app/immutable/chunks/Ds6Is8Xn.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1fql0i9)==null?void 0:s.base)??"/himmelblau";var a;const l=((a=globalThis.__sveltekit_1fql0i9)==null?void 0:a.assets)??e;export{l as a,e as b};
--- _app/immutable/entry/start.Y_rLsow6.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/BezVAwtH.js";export{t as start};
