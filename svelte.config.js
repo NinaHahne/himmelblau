e370bb946ab7c286f131612c04cccddf86c57804
NinaHahne <ninamariehahne@gmail.com>
2025-07-08 16:49:27 +0200

    make paths.base dynamic based on DEPLOY_PLATFORM for Netlify & GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
@@ -13,7 +14,7 @@ const config = {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/himmelblau' : '', // Use '/himmelblau' in production, '' locally
+      base: process.env.DEPLOY_PLATFORM === 'netlify' ? '' : '/himmelblau', // Use '/himmelblau' for GitHub Pages, '' locally and for Netlify
     },
     prerender: {
       handleHttpError: 'warn', // Suppress 404 errors for now
